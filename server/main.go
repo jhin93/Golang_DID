0c94c0768807deb359db0bab0c17d3bd784bb68c
jhin93 <scene1993@gmail.com>
2024-04-22 22:14:06 +0900

    intialize project

---
+++ server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
