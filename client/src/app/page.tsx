74a369c0abcc6300f4f718a3eb449466c3a5a051
jhin93 <scene1993@gmail.com>
2024-04-23 22:54:07 +0900

    style fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -35,13 +35,14 @@ const Container = styled.div`
     justify-content: center;
     height: 100vh;
     background-color: #f0f2f5;
+    color: black;
 `;
 
 const Form = styled.form`
     display: flex;
     flex-direction: column;
     width: 400px; /* 너비 증가 */
-    background: white;
+    // background: white;
     padding: 30px; /* 패딩 증가 */
     border-radius: 8px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
@@ -52,11 +53,13 @@ const Input = styled.input`
     padding: 15px; /* 패딩 증가 */
     border: 1px solid #ccc;
     border-radius: 4px;
+    background: white;
+    color: black;
 `;
 
 const Button = styled.button`
     padding: 15px 30px; /* 버튼 패딩 증가 */
-    color: white;
+    // color: white;
     background-color: #0070f3;
     border: none;
     border-radius: 4px;
